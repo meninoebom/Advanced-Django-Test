771e22d1daf7a39f17ff9d7bd4ebeaf94e3eda40
Brandon Brown <brandon.brown@ymail.com>
2015-05-18 09:56:30 -0700

    created app main_app added a url routed to the home template and generated requirements.txt

--- advanced_django_project/advanced_django_project/urls.py
+++ advanced_django_project/advanced_django_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+import main_app.urls as main_app_urls
 
 urlpatterns = [
+    url(r'^', include(main_app_urls)),
     url(r'^admin/', include(admin.site.urls)),
 ]
